4d4ce875eedf708f885d3e6953b24d28d20e80be
djharrik <djharrik@gmail.com>
2025-01-14 09:11:37 +0530

    solve problem like playing with Variables

---
+++ hands on practise in js foundation/playingWithVariables.js
@@ -0,0 +1,68 @@
+/**
+ * Task:
+
+Perform the following mathematical operations
+on the provided variables a and b
+
+
+
+Add
+
+Subtract
+
+Multiply
+
+Divide
+
+Increment
+
+Decrement
+
+Reminder
+
+
+ */
+// Addition of two values
+const add = (a,b)=>{
+    return a+b;
+}
+// console.log(add(12,2))
+
+// Subtract small value from larger one
+const Subtract = ()=>{
+    return a-b;
+}
+// console.log(add(12,2))
+
+
+const multiplying = (a,b)=>{
+    return a*b;
+}
+// console.log(multiplying(12,2))
+
+// Divide larger value by small
+const divide = (a,b)=>{
+    return a/b;
+}
+// console.log(divide(12,2))
+
+// Increase value of a by 1
+const Increment = (a)=>{
+    return a+1;
+}
+// console.log(divide(12,2))
+
+
+// Decrease value of b by 1
+const Decrement = (a,b)=>{
+    return b-1;
+}
+// console.log(divide(12,2))
+
+
+// Divide larger value by small to find the reminder
+const Reminder= (a,b) =>{
+    return a%b;
+    
+}
+
